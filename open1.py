6418c0f2882f3f9c09fb56a5aecde710f99829e7
Kim Minho <alsghkim97@naver.com>
2023-12-08 06:22:19 +0900

    commit 10
        통계버튼 추가
         -바탕색:white / 글자색:orange
         (함수연동필요)

--- open1.py
+++ open1.py
@@ -41,6 +41,9 @@ hintButton = Button(f1,text="힌트",fg="blue",bg="white")
 hintButton.place(x=461,y=300)
 
 
+histogram_button = tk.Button(window, text="통계",fg="orange",bg="white")
+histogram_button.place(x=497, y=300)
+
 f1.pack(fill="both",expand=True)
 
 
